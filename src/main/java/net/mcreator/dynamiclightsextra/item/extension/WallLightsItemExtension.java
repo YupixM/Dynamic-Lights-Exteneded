c9bf711897661981051c859d94da3c32ffaadb83
YupixM <147986776+YupixM@users.noreply.github.com>
2023-10-16 11:33:24 +0300

    Add files via upload

---
+++ src/main/java/net/mcreator/dynamiclightsextra/item/extension/WallLightsItemExtension.java
@@ -0,0 +1,23 @@
+
+package net.mcreator.dynamiclightsextra.item.extension;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Blocks;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class WallLightsItemExtension {
+	@Mod.EventBusSubscriber
+	public static class Fuel {
+		@SubscribeEvent
+		public static void furnaceFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event) {
+			ItemStack itemstack = event.getItemStack();
+			if (itemstack.getItem() == Blocks.WALL_TORCH.asItem())
+
+				event.setBurnTime(0);
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/dynamiclightsextra/procedures/DynamicbubblecolumnProcedure.java
@@ -0,0 +1,104 @@
+package net.mcreator.dynamiclightsextra.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.state.Property;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.dynamiclightsextra.DynamicLightsExtraMod;
+
+import java.util.Map;
+
+public class DynamicbubblecolumnProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				DynamicLightsExtraMod.LOGGER.warn("Failed to load dependency world for procedure Dynamicbubblecolumn!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				DynamicLightsExtraMod.LOGGER.warn("Failed to load dependency x for procedure Dynamicbubblecolumn!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				DynamicLightsExtraMod.LOGGER.warn("Failed to load dependency y for procedure Dynamicbubblecolumn!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				DynamicLightsExtraMod.LOGGER.warn("Failed to load dependency z for procedure Dynamicbubblecolumn!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		new Object() {
+			private int ticks = 0;
+			private float waitTicks;
+			private IWorld world;
+
+			public void start(IWorld world, int waitTicks) {
+				this.waitTicks = waitTicks;
+				MinecraftForge.EVENT_BUS.register(this);
+				this.world = world;
+			}
+
+			@SubscribeEvent
+			public void tick(TickEvent.ServerTickEvent event) {
+				if (event.phase == TickEvent.Phase.END) {
+					this.ticks += 1;
+					if (this.ticks >= this.waitTicks)
+						run();
+				}
+			}
+
+			private void run() {
+				if (world instanceof World)
+					((World) world).notifyNeighborsOfStateChange(new BlockPos(x, y, z),
+							((World) world).getBlockState(new BlockPos(x, y, z)).getBlock());
+				{
+					BlockPos _bp = new BlockPos(x, y, z);
+					BlockState _bs = Blocks.BUBBLE_COLUMN.getDefaultState();
+					BlockState _bso = world.getBlockState(_bp);
+					for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+						Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+						if (_property != null && _bs.get(_property) != null)
+							try {
+								_bs = _bs.with(_property, (Comparable) entry.getValue());
+							} catch (Exception e) {
+							}
+					}
+					TileEntity _te = world.getTileEntity(_bp);
+					CompoundNBT _bnbt = null;
+					if (_te != null) {
+						_bnbt = _te.write(new CompoundNBT());
+						_te.remove();
+					}
+					world.setBlockState(_bp, _bs, 3);
+					if (_bnbt != null) {
+						_te = world.getTileEntity(_bp);
+						if (_te != null) {
+							try {
+								_te.read(_bso, _bnbt);
+							} catch (Exception ignored) {
+							}
+						}
+					}
+				}
+				MinecraftForge.EVENT_BUS.unregister(this);
+			}
+		}.start(world, (int) 5);
+	}
+}
